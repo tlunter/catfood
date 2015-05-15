510569eaad6c6a2ef84d324ca56f01881c37ec6e
Todd Lunter <tlunter@gmail.com>
2015-05-14 21:21:57 -0400

    Can create new recipes

--- assets/js/home.jsx
+++ assets/js/home.jsx
@@ -1,6 +1,11 @@
 var Home = React.createClass({
   getInitialState: function() {
-    return { recipes: [] };
+    return {
+      recipes: [],
+      name: "",
+      description: "",
+      cookTime: 0,
+    };
   },
   componentDidMount: function() {
     this.load();
@@ -14,16 +19,78 @@ var Home = React.createClass({
       }.bind(this)
     });
   },
+
+  handleSubmit: function(event) {
+    event.preventDefault();
+
+    reqwest({
+      url: '/recipes/',
+      method: 'post',
+      type: 'json',
+      data: JSON.stringify({
+        name: this.state.name,
+        description: this.state.description,
+        cookTime: this.state.cookTime
+      }),
+      contentType: 'application/json',
+      success: function(res) {
+        this.load();
+        this.setState({
+          name: '',
+          description: '',
+          cookTime: 0
+        });
+      }.bind(this)
+    });
+  },
+
+  handleOnChange: function(field) {
+    return function(field, event) {
+      var updates = {};
+      updates[field] = event.target.value;
+      this.setState(updates);
+    }.bind(this, field);
+  },
+
   render: function() {
     var recipes = this.state.recipes.map(function(recipe) {
-      return <tr><td>{recipe.name}</td><td>{recipe.description}</td></tr>;
+      return (
+        <tr>
+          <td>{recipe.name}</td><td>{recipe.description}</td><td>{recipe.cookTime}</td>
+        </tr>
+      );
     });
     return (
-      <table>
-        <tbody>
-          {recipes}
-        </tbody>
-      </table>
+      <form onSubmit={this.handleSubmit}>
+        <table>
+          <thead>
+            <tr>
+              <td>Name:</td>
+              <td>Description:</td>
+              <td>Cooktime:</td>
+            </tr>
+          </thead>
+          <tbody>
+            {recipes}
+          </tbody>
+          <tbody>
+            <tr>
+              <td>
+                <input type="text" value={this.state.name} onChange={this.handleOnChange('name')} />
+              </td>
+              <td>
+                <input type="text" value={this.state.description} onChange={this.handleOnChange('description')} />
+              </td>
+              <td>
+                <input type="text" value={this.state.cookTime} onChange={this.handleOnChange('cookTime')} />
+              </td>
+              <td>
+                <button onClick={this.handleSubmit}>Add!</button>
+              </td>
+            </tr>
+          </tbody>
+        </table>
+      </form>
     );
   }
 });
